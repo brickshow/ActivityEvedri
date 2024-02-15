49da67a9dfe960abfb3110840b784df3a936253d
brickshow <act.blampago@gmail.com>
2024-02-15 09:40:02 +0800

    final

--- LabExercise4/LABSW3/LogInForm/LogInForm/LogIn.Designer.cs
+++ LabExercise4/LABSW3/LogInForm/LogInForm/LogIn.Designer.cs
@@ -36,9 +36,15 @@
             this.picboxDesignTop = new System.Windows.Forms.PictureBox();
             this.picboxLogo = new System.Windows.Forms.PictureBox();
             this.lblIndicator = new System.Windows.Forms.Label();
+            this.lblEmptyUser = new System.Windows.Forms.Label();
+            this.lblEmptyPass = new System.Windows.Forms.Label();
+            this.pboxPassworShow = new System.Windows.Forms.PictureBox();
+            this.pboxHidePassword = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.picboxDesignbottom)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picboxDesignTop)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picboxLogo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxPassworShow)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxHidePassword)).BeginInit();
             this.SuspendLayout();
             // 
             // txtboxUsername
@@ -77,6 +83,7 @@
             this.btnLogIn.TabIndex = 4;
             this.btnLogIn.Text = "LOG IN";
             this.btnLogIn.UseVisualStyleBackColor = false;
+            this.btnLogIn.Click += new System.EventHandler(this.btnLogIn_Click);
             // 
             // picboxDesignbottom
             // 
@@ -116,11 +123,57 @@
             this.lblIndicator.BackColor = System.Drawing.Color.Transparent;
             this.lblIndicator.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblIndicator.ForeColor = System.Drawing.Color.Red;
-            this.lblIndicator.Location = new System.Drawing.Point(110, 363);
+            this.lblIndicator.Location = new System.Drawing.Point(105, 244);
             this.lblIndicator.Name = "lblIndicator";
             this.lblIndicator.Size = new System.Drawing.Size(215, 15);
             this.lblIndicator.TabIndex = 8;
             this.lblIndicator.Text = "Username and password don\'t match!";
+            // 
+            // lblEmptyUser
+            // 
+            this.lblEmptyUser.AutoSize = true;
+            this.lblEmptyUser.BackColor = System.Drawing.Color.Transparent;
+            this.lblEmptyUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmptyUser.ForeColor = System.Drawing.Color.Red;
+            this.lblEmptyUser.Location = new System.Drawing.Point(151, 294);
+            this.lblEmptyUser.Name = "lblEmptyUser";
+            this.lblEmptyUser.Size = new System.Drawing.Size(129, 15);
+            this.lblEmptyUser.TabIndex = 8;
+            this.lblEmptyUser.Text = "Username is required!";
+            // 
+            // lblEmptyPass
+            // 
+            this.lblEmptyPass.AutoSize = true;
+            this.lblEmptyPass.BackColor = System.Drawing.Color.Transparent;
+            this.lblEmptyPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmptyPass.ForeColor = System.Drawing.Color.Red;
+            this.lblEmptyPass.Location = new System.Drawing.Point(155, 360);
+            this.lblEmptyPass.Name = "lblEmptyPass";
+            this.lblEmptyPass.Size = new System.Drawing.Size(125, 15);
+            this.lblEmptyPass.TabIndex = 8;
+            this.lblEmptyPass.Text = "Password is required!";
+            // 
+            // pboxPassworShow
+            // 
+            this.pboxPassworShow.BackColor = System.Drawing.Color.White;
+            this.pboxPassworShow.Image = ((System.Drawing.Image)(resources.GetObject("pboxPassworShow.Image")));
+            this.pboxPassworShow.Location = new System.Drawing.Point(346, 331);
+            this.pboxPassworShow.Name = "pboxPassworShow";
+            this.pboxPassworShow.Size = new System.Drawing.Size(25, 21);
+            this.pboxPassworShow.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pboxPassworShow.TabIndex = 9;
+            this.pboxPassworShow.TabStop = false;
+            // 
+            // pboxHidePassword
+            // 
+            this.pboxHidePassword.BackColor = System.Drawing.Color.White;
+            this.pboxHidePassword.Image = ((System.Drawing.Image)(resources.GetObject("pboxHidePassword.Image")));
+            this.pboxHidePassword.Location = new System.Drawing.Point(346, 332);
+            this.pboxHidePassword.Name = "pboxHidePassword";
+            this.pboxHidePassword.Size = new System.Drawing.Size(25, 21);
+            this.pboxHidePassword.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pboxHidePassword.TabIndex = 9;
+            this.pboxHidePassword.TabStop = false;
             // 
             // LogIn
             // 
@@ -128,6 +181,10 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(434, 617);
+            this.Controls.Add(this.pboxPassworShow);
+            this.Controls.Add(this.pboxHidePassword);
+            this.Controls.Add(this.lblEmptyPass);
+            this.Controls.Add(this.lblEmptyUser);
             this.Controls.Add(this.lblIndicator);
             this.Controls.Add(this.picboxLogo);
             this.Controls.Add(this.btnLogIn);
@@ -139,9 +196,12 @@
             this.Name = "LogIn";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "LogIn";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.LogIn_KeyPress);
             ((System.ComponentModel.ISupportInitialize)(this.picboxDesignbottom)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.picboxDesignTop)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.picboxLogo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxPassworShow)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxHidePassword)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -155,6 +215,10 @@
         private System.Windows.Forms.PictureBox picboxDesignTop;
         private System.Windows.Forms.PictureBox picboxLogo;
         private System.Windows.Forms.Label lblIndicator;
+        private System.Windows.Forms.Label lblEmptyUser;
+        private System.Windows.Forms.Label lblEmptyPass;
+        private System.Windows.Forms.PictureBox pboxPassworShow;
+        private System.Windows.Forms.PictureBox pboxHidePassword;
     }
 }
 
--- LabExercise4/LABSW3/LogInForm/LogInForm/LogIn.cs
+++ LabExercise4/LABSW3/LogInForm/LogInForm/LogIn.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics.Eventing.Reader;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,7 +16,55 @@ namespace LogInForm
         public LogIn()
         {
             InitializeComponent();
+
+            //Error Indicator
             lblIndicator.Hide();
+            lblEmptyPass.Hide();
+            lblEmptyUser.Hide();
+
+            //Code by using lambda expressions directly as event handlers
+            txtboxUsername.Enter += (sender, e) => {
+                if (txtboxUsername.Text == "Username")
+                {
+                    txtboxUsername.Clear();
+                    txtboxUsername.ForeColor = SystemColors.ControlText;
+                }
+            };
+            txtboxUsername.Leave += (sender, e) => {
+                if (string.IsNullOrWhiteSpace(txtboxUsername.Text))
+                {
+                    txtboxUsername.Text = "Username";
+                    txtboxUsername.ForeColor = SystemColors.GrayText;
+                }
+            };
+            txtboxPassword.Enter += (sender, e) => {
+                if (txtboxPassword.Text == "Password")
+                {
+                    txtboxPassword.Clear();
+                    txtboxPassword.ForeColor = SystemColors.ControlText;
+                }
+            };
+            txtboxPassword.Leave += (sender, e) => {
+                if (string.IsNullOrWhiteSpace(txtboxPassword.Text))
+                {
+                    txtboxPassword.Text = "Password";
+                    txtboxPassword.ForeColor = SystemColors.GrayText;
+                }
+            };
+
+            //Lambda expression for show and hide password
+            pboxPassworShow.MouseDown += (sender, e) =>
+            {
+                txtboxPassword.PasswordChar = '\0';
+                pboxHidePassword.BringToFront();
+            };
+            pboxPassworShow.MouseUp += (sender, e) =>
+            {
+                pboxPassworShow.BringToFront();
+                txtboxPassword.PasswordChar = '•';
+            };
+
+            this.KeyPreview = true;//Enable the key preview for the form
         }
 
         private void txtboxUsername_KeyPress(object sender, KeyPressEventArgs e)
@@ -23,17 +72,39 @@ namespace LogInForm
             //Execute the enter events
             if (e.KeyChar == (char)Keys.Enter) 
             {
-                Control ctrNext = ActiveControl;
-                if (ctrNext == txtboxPassword) logIn();
-                SelectNextControl(ctrNext,true, false, true, true);
+                Control actCtr = ActiveControl;
+                if (actCtr == txtboxPassword) logIn();
+                SelectNextControl(actCtr, true, true, true, true);
             } 
         }
 
         //Log In method
         public void logIn()
         {
-            if (txtboxUsername.Text == "admin" && txtboxPassword.Text == "admin") MessageBox.Show("Successfully Logged In");
-            else lblIndicator.Show(); txtboxUsername.Focus();
+            if (txtboxUsername.Text == "admin" && txtboxPassword.Text == "admin")
+                MessageBox.Show("Successfully Logged In");
+            else if (txtboxUsername.Text == "") lblEmptyUser.Show();
+            else if (txtboxPassword.Text == "") lblEmptyPass.Show();
+            else lblIndicator.Show();
+        }
+
+        private void btnLogIn_Click(object sender, EventArgs e)
+        {
+            //Do the log in Using button
+            logIn();
+        }
+
+        private void LogIn_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //Check if the user used escape keys
+            if (e.KeyChar == (char)Keys.Escape)
+            {
+                //Show a confirmation
+                DialogResult result = MessageBox.Show("Are you sure you want to leave?", "Please confirm", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+                
+                //Check if the user confirm using OK button
+                if (result == DialogResult.OK) this.Close(); 
+            }
         }
     }
 }
